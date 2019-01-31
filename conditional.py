3eaec3c82ebebed98c1d25baa6fa7c4e8c613101
wikislate <wikislate@gmail.com>
2019-01-31 16:56:21 +0000

    Conditional

---
+++ conditional.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+a, b = 4, 1
+
+if a < b:
+	print('a ({}) is less than b ({})'.format(a, b))
+else:
+	print('a ({}) is not less than b ({})'.format(a, b))
---
+++ if_else_oneline.py
@@ -0,0 +1,3 @@
+#!/usr/local/bin/python3
+a, b = 3, 6
+print("a is small" if a < b else "a is not small")
